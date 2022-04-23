103c5fb1d40ca37bee2b527bb85ba915d5990e79
shirve <43320967+shirve@users.noreply.github.com>
2022-04-23 19:11:36 +0200

    listing details page

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import CreateListing from './pages/CreateListing'
+import Listing from './pages/Listing'
 
 function App() {
   return (
@@ -21,6 +22,10 @@ function App() {
           <Route path='/' element={<Explore />} />
           <Route path='/offers' element={<Offers />} />
           <Route path='/category/:categoryName' element={<Category />} />
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
