1ef76ecbcef0c5cc7c5b612f11698dcbdc7fd187
shirve <43320967+shirve@users.noreply.github.com>
2022-04-26 19:15:01 +0200

    hid firebase config

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,14 +1,13 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDPLw2uhepdYN061OAzOamuxQiVg66Bfg0',
-  authDomain: 'house-marketplace-f6b05.firebaseapp.com',
-  projectId: 'house-marketplace-f6b05',
-  storageBucket: 'house-marketplace-f6b05.appspot.com',
-  messagingSenderId: '10413331891',
-  appId: '1:10413331891:web:b5761b74ca949b714fa7c6',
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 }
 
 // Initialize Firebase
